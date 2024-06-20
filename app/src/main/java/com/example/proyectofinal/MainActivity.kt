004fc96bd7abdf6b8d93ef0cf8445667fb8c5767
Dante Gutierrez <2121200441@soy.utj.edu.mx>
2024-06-20 17:43:12 -0600

    v1.0.2 Correcion aplicacion

--- app/src/main/java/com/example/proyectofinal/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinal/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.proyectofinal
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+//    lateinit var BtnPrincipal: Button
+//    lateinit var BtnRegistro: Button
+//    lateinit var EdtUsuario: EditText
+//    lateinit var EdtContraseña: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+//
+//        BtnPrincipal.findViewById<Button>(R.id.btnInicio)
+//        BtnRegistro.findViewById<Button>(R.id.btnRegistro)
+//        EdtUsuario.findViewById<EditText>(R.id.edtUsuario)
+//        EdtContraseña.findViewById<EditText>(R.id.edtpPassword)
     }
+
+//    fun principal(vista: View){
+//        if (EdtUsuario.text.isEmpty() || EdtContraseña.text.isEmpty()){
+//            Toast.makeText(this, "Usuario o contraseña no validos.", Toast.LENGTH_SHORT).show()
+//
+//        }else{
+//            Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+//            val intent = Intent(this, Inicio::class.java)
+//            startActivity(intent)
+//            finish()
+//            true
+//        }
+//    }//principal
+
+//    fun Registro(vista: View){
+//        val intent = Intent(this, Registro::class.java)
+//        startActivity(intent)
+//        finish()
+//        true
+//    }//Registro
 }
