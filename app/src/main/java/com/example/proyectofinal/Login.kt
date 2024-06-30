f32db71d5dda87876f122cb7785b6358f1f4f7a3
Dante Gutierrez <2121200441@soy.utj.edu.mx>
2024-06-30 17:04:55 -0600

    v1.0.7 Muestra para la presentación de proyecto

--- app/src/main/java/com/example/proyectofinal/Login.kt
+++ app/src/main/java/com/example/proyectofinal/Login.kt
@@ -35,7 +35,7 @@ class Login : AppCompatActivity() {
 
         }else{
             Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, Inicio::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
             true
