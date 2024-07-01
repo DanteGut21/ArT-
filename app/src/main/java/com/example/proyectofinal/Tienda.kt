411eca6166326b886c67ea5d010e8a385c6091ae
Dante Gutierrez <2121200441@soy.utj.edu.mx>
2024-06-30 22:38:04 -0600

    v1.0.14 Implementación Fragment producto

--- app/src/main/java/com/example/proyectofinal/Tienda.kt
+++ app/src/main/java/com/example/proyectofinal/Tienda.kt
@@ -1,22 +1,33 @@
 package com.example.proyectofinal
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 
 class Tienda : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tienda, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_tienda, container, false)
+
+        // Configura el clic en el LinearLayout "ItemTienda"
+        val itemTienda: LinearLayout = view.findViewById(R.id.ItemTienda)
+        itemTienda.setOnClickListener {
+            // Transacción de fragmentos
+            val producto = Producto()
+            val transaction: FragmentTransaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, producto)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+
+        return view
+    }//onCreateView
 }//Class Tienda
