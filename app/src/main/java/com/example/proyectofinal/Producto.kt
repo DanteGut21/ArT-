e1a9722e61c1a5df2fcfc362f1846e0592bb63bd
Dante Gutierrez <2121200441@soy.utj.edu.mx>
2024-07-14 23:48:17 -0600

    v1.0.18 implementación opciones de pago

--- app/src/main/java/com/example/proyectofinal/Producto.kt
+++ app/src/main/java/com/example/proyectofinal/Producto.kt
@@ -1,9 +1,12 @@
 package com.example.proyectofinal
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class Producto : Fragment() {
@@ -13,7 +16,14 @@ class Producto : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_producto, container, false)
+        val view = inflater.inflate(R.layout.fragment_producto, container, false)
+        val btnPago: Button = view.findViewById(R.id.btnPago)
+        btnPago.setOnClickListener {
+            // Aquí manejarías el pago por transferencia bancaria
+            Toast.makeText(context, "Pagos...", Toast.LENGTH_SHORT).show()
+            val intent = Intent(activity, PagoProducto::class.java)
+            startActivity(intent)
+        }
+        return view
     }
-
 }//Class Producto
