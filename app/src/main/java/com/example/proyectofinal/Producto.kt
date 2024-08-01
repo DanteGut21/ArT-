8d14ce0b7f3aa3c5ad21231ea7c831ebcc82226e
Dante Gutierrez <2121200441@soy.utj.edu.mx>
2024-07-31 22:57:27 -0600

    v1.0.35 implementación modal para los pagos

--- app/src/main/java/com/example/proyectofinal/Producto.kt
+++ app/src/main/java/com/example/proyectofinal/Producto.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 
 class Producto : Fragment() {
 
@@ -20,16 +19,16 @@ class Producto : Fragment() {
         val btnPago: Button = view.findViewById(R.id.btnPago)
         val btnCarrito: Button = view.findViewById(R.id.btnCarrito)
         btnPago.setOnClickListener {
-            // Aquí manejarías el pago por transferencia bancaria
-            Toast.makeText(context, "Pagos...", Toast.LENGTH_SHORT).show()
-//            val intent = Intent(activity, PagoProducto::class.java)
-//            startActivity(intent)
+            Toast.makeText(context, "Preparando opciones de pago...", Toast.LENGTH_SHORT).show()
 
-            val pago = Pago()
-            val transaction: FragmentTransaction = parentFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment_container, pago)
-            transaction.addToBackStack(null)
-            transaction.commit()
+            // Creando una instancia del fragmento de pago
+            val pagoFragment = Pago()
+
+            // Realizando la transacción para añadir el fragmento de pago sobre el fragmento actual
+            parentFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, pagoFragment)
+                .addToBackStack(null) // Permite al usuario volver al fragmento anterior
+                .commit()
         }
         btnCarrito.setOnClickListener {
             Toast.makeText(context, "Carrito", Toast.LENGTH_SHORT).show()
